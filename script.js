31dbf3bd7835480dd78649ca9d0449372053dd13
tasnim-h <t.hussien@share.epsb.ca>
2024-11-28 14:10:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// JavaScript IPO Written Response
+
+// Question 1 (7 marks)
+
+// The user types any 3 letter word in the input. Then, in the box below, their name appears, surrounded by stars. For example:
+
+//    *********
+//    ***CAT***
+//    *********
+
+// Question 2 (8 marks)
+
+// When the user clicks the button, these 4 things happen:
+//  - The image changes from hulk to captain america (the images are already in the images folder)
+//  - The h2 should say "Captain America" instead of "The Incredible Hulk"
+//  - The site background changes to blue (this is the color along the sides that is currently green)
+//  - The border color around the image changes to blue
+
+
+
+
+
+
+function bluenn() {
+  var element = document.body;
+  element.classList.toggle(".blue");
+}
+
+// Question 3 (10 marks)
+
+// When the user clicks the button, take the numbers typed in the radius, height, and slant inputs, and calculate both the volume and area of the cone.  Then output the results to the appropriate spans. Round to 2 decimal places. Use either 3.14 or Math.PI for your calculations.
